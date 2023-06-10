ec17a944c69fd3a313d63aa3f80b5c944f42a024
PabloLaz <aplaz1999@gmail.com>
2023-06-11 00:12:32 +0200

    Commit

--- src/main/java/com/eoi/ejemplospringboot/entities/Reparacion.java
+++ src/main/java/com/eoi/ejemplospringboot/entities/Reparacion.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.Set;
 
 
 @Getter
@@ -17,9 +21,35 @@ public class Reparacion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    @Column
-    private String name;
+    private Integer id;
 
+    // El FetchType indica cómo se cargará los datos
+    // en este caso, se cargarán inmediatamente
+    // de lo contrario, sería tipo LAZY en lugar de EAGER
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Usuario> usuarios;
+
+    @ManyToOne
+    private Cliente cliente;
+
+//    @ManyToOne
+//    private Coche coche;
+//
+//    @ManyToOne
+//    private Tipo tipo;
+
+    @Column
+    private String descripcion;
+
+    @Column
+    private String duracion;
+
+    @Column
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDateTime fechaDeInicio;
+
+    @Column
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDateTime fechaDeFin;
 
 }
