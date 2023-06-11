48bd202408e23c3c48c3d12263291f1c066e5f85
DanielBF99 <bilbaofornelldaniel@gmail.com>
2023-06-11 19:17:33 +0200

    CRUD de empleado

--- src/main/java/com/eoi/ejemplospringboot/controllers/EmpleadoController.java
+++ src/main/java/com/eoi/ejemplospringboot/controllers/EmpleadoController.java
@@ -1,30 +1,62 @@
 package com.eoi.ejemplospringboot.controllers;
 
+import com.eoi.ejemplospringboot.entities.Empleado;
 import com.eoi.ejemplospringboot.services.EmpleadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/empleados")
 public class EmpleadoController {
 
+    private final EmpleadoService empleadoService;
     @Autowired
-    EmpleadoService EmpleadoService;
+    public EmpleadoController(EmpleadoService empleadoService) {
+        this.empleadoService = empleadoService;
+    }
 
     @GetMapping("/all")
-    public String all(Model model) {
-        model.addAttribute("entities",EmpleadoService.findAll());
+    public String getAllEmpleados(Model model) {
+        model.addAttribute("entities",empleadoService.findAll());
         return "empleados";
     }
 
-    @GetMapping("/{id}")
-    public String all(@PathVariable Integer id, Model model) {
-        model.addAttribute("entity", EmpleadoService.findById(id));
-        return "empleados/entity-details";
+    // Delete
+    @GetMapping("{id}/delete")
+    public String deleteEmpleadoById(
+            @PathVariable("id") Integer id) {
+        empleadoService.deleteEmpleadoById(id);
+        return "redirect:/empleados/all";
+    }
+    // Create
+    @GetMapping("new")
+    public String addEmpleadoForm(Model model) {
+        model.addAttribute("empleado", new Empleado());
+        return "empleados/empleado-create";
     }
 
+    @PostMapping("/create")
+    public String createEmpleado(@ModelAttribute("empleado") Empleado empleado) {
+        empleadoService.createEmpleado(empleado);
+        return "redirect:/empleados/all";
+    }
+
+    // Update & Show
+    @GetMapping("/{id}")
+    public String updateEmpleadoForm(
+            @PathVariable("id") Integer id, Model model) {
+        model.addAttribute("entity", empleadoService.findById(id));
+        Optional<Empleado> empleado = empleadoService.findById(id);
+        empleado.ifPresent(value -> model.addAttribute("empleado", value));
+        return "empleados/empleado-update";
+    }
+    @PostMapping("/{id}")
+    public String updateEmpleado(@ModelAttribute Empleado empleado){
+        empleadoService.updateEmpleado(empleado);
+        return "redirect:/empleados/all";
+    }
 }
--- src/main/java/com/eoi/ejemplospringboot/repositories/EmpleadoRepository.java
+++ src/main/java/com/eoi/ejemplospringboot/repositories/EmpleadoRepository.java
@@ -1,9 +1,13 @@
 package com.eoi.ejemplospringboot.repositories;
 
 import com.eoi.ejemplospringboot.entities.Empleado;
-import com.eoi.ejemplospringboot.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
+
+    Optional<Empleado> findByNombre(String empleadoname);
 
 }
