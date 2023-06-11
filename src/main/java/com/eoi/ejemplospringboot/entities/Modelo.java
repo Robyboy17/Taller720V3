a25b084c6d61c5888aa330297f3d8a89add22ebf
PabloLaz <aplaz1999@gmail.com>
2023-06-11 03:20:12 +0200

    Funcionalidades de modelos
    Funcionalidades de modelos

---
+++ src/main/java/com/eoi/ejemplospringboot/entities/Modelo.java
@@ -0,0 +1,30 @@
+package com.eoi.ejemplospringboot.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Modelo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String nombre;
+
+//    @Column
+//    @ManyToOne(fetch = FetchType.EAGER)
+//    private Marca marca;
+
+
+}
+
