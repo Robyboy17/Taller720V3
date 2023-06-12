ae6520161e87179d1ae6b6a4327542201a6e1354
PabloLaz <aplaz1999@gmail.com>
2023-06-12 03:49:22 +0200

    Descomento las otras clases

--- src/main/java/com/eoi/ejemplospringboot/services/EmpresaService.java
+++ src/main/java/com/eoi/ejemplospringboot/services/EmpresaService.java
@@ -1,28 +1,28 @@
-//package com.eoi.ejemplospringboot.services;
-//
-//import com.eoi.ejemplospringboot.entities.Empresa;
-//import com.eoi.ejemplospringboot.entities.Usuario;
-//import com.eoi.ejemplospringboot.repositories.EmpresaRepository;
-//import com.eoi.ejemplospringboot.repositories.UsuarioRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class EmpresaService {
-//
-//    @Autowired
-//    EmpresaRepository repository;
-//
-//    public List<Empresa> findAll() {
-//        return repository.findAll();
-//    }
-//
-//    public Optional<Empresa> findById(Integer id) {
-//        return repository.findById(id);
-//    }
-//
-//
-//}
+package com.eoi.ejemplospringboot.services;
+
+import com.eoi.ejemplospringboot.entities.Empresa;
+import com.eoi.ejemplospringboot.entities.Usuario;
+import com.eoi.ejemplospringboot.repositories.EmpresaRepository;
+import com.eoi.ejemplospringboot.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmpresaService {
+
+    @Autowired
+    EmpresaRepository repository;
+
+    public List<Empresa> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Empresa> findById(Integer id) {
+        return repository.findById(id);
+    }
+
+
+}
