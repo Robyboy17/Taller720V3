2f6846e0118eb7d9169f209875ddd550e1bd692b
Robert Lungu <robyboy17@gmail.com>
2023-06-12 13:41:30 +0200

    Correcciones

--- src/main/java/com/eoi/ejemplospringboot/repositories/EmpleadoRepository.java
+++ src/main/java/com/eoi/ejemplospringboot/repositories/EmpleadoRepository.java
@@ -2,12 +2,7 @@ package com.eoi.ejemplospringboot.repositories;
 
 import com.eoi.ejemplospringboot.entities.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
 
 public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
-
-    Optional<Empleado> findByNombre(String empleadoname);
-
 }
