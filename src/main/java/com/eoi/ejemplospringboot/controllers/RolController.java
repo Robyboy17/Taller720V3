dd8a5960c8d938ee644630669971573c5e42672c
Robert Lungu <robyboy17@gmail.com>
2023-06-13 15:44:57 +0200

    correcciones

--- src/main/java/com/eoi/ejemplospringboot/controllers/RolController.java
+++ src/main/java/com/eoi/ejemplospringboot/controllers/RolController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/Rol")
+@RequestMapping("/roles")
 public class RolController {
 
     private final RolService rolService;
--- src/main/java/com/eoi/ejemplospringboot/entities/Rol.java
+++ src/main/java/com/eoi/ejemplospringboot/entities/Rol.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
-import java.util.Collection;
 import java.util.Set;
 
 @Getter
